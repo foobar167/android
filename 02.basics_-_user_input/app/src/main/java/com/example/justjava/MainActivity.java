8c15058933bd4e5fa339caaf2afe51c8e70399b2
FooBar167 <foobar167@gmail.com>
2019-10-06 23:58:32 +0300

    Software update

--- 02.basics_-_user_input/app/src/main/java/com/example/justjava/MainActivity.java
+++ 02.basics_-_user_input/app/src/main/java/com/example/justjava/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.justjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import java.util.Locale;
 
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        quantity += 1;
+        display(quantity);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = findViewById(R.id.text_number);
+        quantityTextView.setText(String.format(Locale.getDefault(), "%d", number));
     }
 }
