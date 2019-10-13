58c76b8ac0f683dab2f42e1ee02a7c61004ab87b
FooBar167 <foobar167@gmail.com>
2019-10-13 16:37:52 +0300

    Software update

--- 02.basics_-_user_input/app/src/main/java/com/example/justjava/MainActivity.java
+++ 02.basics_-_user_input/app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,25 +1,38 @@
 package com.example.justjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 import java.util.Locale;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-    int quantity = 0;
+    static int quantity = 0;  // order quantity
+    static double price = 5;  // $5 for 1 cup of coffee
+    NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);  // set dollars currency
 
+    /**
+     * This method is called when the widget is created.
+     *
+     * @param savedInstanceState - I don't know what is it :-)
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        display(quantity);  // display quantity again
     }
 
     /**
      * This method is called when the order button is clicked.
+     *
+     * @param view - view of the main screen
      */
     public void submitOrder(View view) {
         quantity += 1;
@@ -28,9 +41,20 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * This method displays the given quantity value on the screen.
+     *
+     * @param number - number to display
      */
     private void display(int number) {
-        TextView quantityTextView = findViewById(R.id.text_number);
+        TextView quantityTextView = findViewById(R.id.text_quantity_number);
         quantityTextView.setText(String.format(Locale.getDefault(), "%d", number));
+        displayPrice(number * price);  // display price after displaying quantity
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(double number) {
+        TextView priceTextView = (TextView) findViewById(R.id.text_price_number);
+        priceTextView.setText(nf.format(number));
     }
 }
