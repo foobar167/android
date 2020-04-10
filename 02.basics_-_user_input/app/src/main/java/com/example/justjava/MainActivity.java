100cbedc23542019f48ef2fa4f7f740428fd2529
foobar167 <foobar167@gmail.com>
2020-04-10 22:52:20 +0300

    Android

--- 02.basics_-_user_input/app/src/main/java/com/example/justjava/MainActivity.java
+++ 02.basics_-_user_input/app/src/main/java/com/example/justjava/MainActivity.java
@@ -52,9 +52,28 @@ public class MainActivity extends AppCompatActivity {
      * @param view - view of the main screen
      */
     public void submitOrder(View view) {
-        orderMessage = "Thank you!";  // set order message to "Thank you!"
-        // Display price when "Order" button is pressed
-        displayPrice(cupsQuantity * COFFEE_PRICE);
+        NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);  // set dollars currency
+        String cups = " cup ";
+        if (cupsQuantity > 1) {
+            cups = " cups ";
+        }
+        orderMessage = "Ordered " + cupsQuantity +
+                cups + "of coffee for " +
+                nf.format(cupsQuantity * COFFEE_PRICE) +
+                ". Thank you!";  // set order message
+        cupsQuantity = 0;
+        display();
+    }
+
+    /**
+     * This method is called when the cancel button is clicked.
+     *
+     * @param view - view of the main screen
+     */
+    public void cancelOrder(View view) {
+        cupsQuantity = 0;
+        orderMessage = "";
+        display();
     }
 
     /**
@@ -63,37 +82,34 @@ public class MainActivity extends AppCompatActivity {
     private void display() {
         Button order = findViewById(R.id.button_order);  // order button
         Button decrease = findViewById(R.id.button_decrease);  // decrease button
-        if(cupsQuantity <= 0) {  // order only positive cups of coffee
+        Button cancel = findViewById(R.id.button_cancel);  // cancel button
+        if (cupsQuantity <= 0) {  // order only positive cups of coffee
             cupsQuantity = 0;  // cups quantity can be >= 0
             order.setEnabled(false);  // disable button
             decrease.setEnabled(false);  // disable button
+            cancel.setEnabled(false);  // disable button
             order.setTextColor(getApplication().getResources().getColor(
                     android.R.color.darker_gray));  // change button color
             decrease.setTextColor(getApplication().getResources().getColor(
                     android.R.color.darker_gray));  // change button color
+            cancel.setTextColor(getApplication().getResources().getColor(
+                    android.R.color.darker_gray));  // change button color
         } else {  // cupsQuantity > 0
             order.setEnabled(true);  // enable button
             decrease.setEnabled(true);  // enable button
+            cancel.setEnabled(true);  // enable button
             // Return old button themes
             order.setTextAppearance(getApplicationContext(), R.style.ButtonTheme);
             decrease.setTextAppearance(getApplicationContext(), R.style.ButtonSquare);
+            cancel.setTextAppearance(getApplicationContext(), R.style.ButtonTheme);
         }
         // Update quantity number text view
         TextView quantityTextView = findViewById(R.id.text_quantity_number);
         quantityTextView.setText(String.format(Locale.getDefault(), "%d", cupsQuantity));
         // Display price automatically after quantity of cups is changed
-        displayPrice(cupsQuantity * COFFEE_PRICE);
-    }
-
-    /**
-     * This method displays the total price on the screen.
-     *
-     * @param number - price to display
-     */
-    private void displayPrice(double number) {
         TextView priceTextView = findViewById(R.id.text_price_number);
         NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);  // set dollars currency
-        priceTextView.setText(nf.format(number));
+        priceTextView.setText(nf.format(cupsQuantity * COFFEE_PRICE));
         // Set order message
         TextView orderTextView = findViewById(R.id.text_order_message);
         orderTextView.setText(orderMessage);
